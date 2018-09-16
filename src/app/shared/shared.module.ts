114310aa1dcaa16ff027c06acbef38a58b81e586
Kelvin Fichter <kelvinfichter@gmail.com>
2018-09-16 19:16:49 -0400

    Added address list component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,14 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AddressListComponent } from './components/address-list/address-list.component';
 
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
  */
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [SidebarComponent],
-  exports: [RouterModule, CommonModule, FormsModule, SidebarComponent]
+  declarations: [
+    SidebarComponent,
+    AddressListComponent
+  ],
+  exports: [
+    RouterModule,
+    CommonModule,
+    FormsModule,
+    SidebarComponent,
+    AddressListComponent
+  ]
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders {
