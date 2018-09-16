7211c342b31a3a61e189da8f6e25c48dd59a4562
Kelvin Fichter <kelvinfichter@gmail.com>
2018-09-16 19:08:35 -0400

    Added sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,17 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
  */
 @NgModule({
-  imports: [CommonModule],
-  exports: [CommonModule, FormsModule]
+  imports: [CommonModule, RouterModule],
+  declarations: [SidebarComponent],
+  exports: [RouterModule, CommonModule, FormsModule, SidebarComponent]
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders {
